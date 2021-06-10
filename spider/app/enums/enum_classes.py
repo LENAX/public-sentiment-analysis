dbcbcdc4b640dd813e7962e9ad97a80404d44346
stevelan <stevelan1995@gmail.com>
2021-06-10 15:28:14 +0800

    updated enum: RequestStatus

--- spider/app/enums/enum_classes.py
+++ spider/app/enums/enum_classes.py
@@ -8,6 +8,53 @@ class JobState(str, Enum):
     FAILED = 'failed'
 
 
+class RequestStatus(str, Enum):
+    """ Maps common HTTP status codes to their corresponding meanings
+    """
+    CREATED = 'created'
+    WAITING = 'waiting'
+    SUCCESS = 'success'
+    TIMEOUT = 'timeout'
+    CLIENT_ERROR = 'client_error'
+    SERVER_ERROR = 'server_error'
+    BAD_REQUEST = 'bad_request'
+    UNAUTHORIZED = 'unauthorized'
+    FORBIDDEN = 'forbidden'
+    NOT_FOUND = 'not_found'
+    INTERNAL_SERVER_ERROR = 'internal_server_error'
+    TOO_MANY_REQUESTS = 'too_many_requests'
+    REDIRECTED = 'redirected'
+
+    @classmethod
+    def from_status_code(cls, status_code: int):
+        """ Convert a status code to its string representation """
+        # checks whether status code is between 200 and 206, but
+        # range is exclusive on the right hand side
+        if 200 <= status_code <= 206:
+            return cls.SUCCESS
+        elif 300 <= status_code <= 309:
+            return cls.REDIRECTED
+        elif status_code == 400:
+            return cls.BAD_REQUEST
+        elif status_code == 401:
+            return cls.UNAUTHORIZED
+        elif status_code == 403:
+            return cls.FORBIDDEN
+        elif status_code == 404:
+            return cls.NOT_FOUND
+        elif status_code == 429:
+            return cls.TOO_MANY_REQUESTS
+        elif 405 <= status_code <= 452:
+            return cls.CLIENT_ERROR
+        elif status_code == 429:
+            return cls.TOO_MANY_REQUESTS
+        elif status_code == 500:
+            return cls.INTERNAL_SERVER_ERROR
+        elif 501 <= status_code <= 511:
+            return cls.SERVER_ERROR
+        
+
+
 class ContentType(str, Enum):
     WEBPAGE: str = 'webpage'
     IMAGE: str = 'image'
