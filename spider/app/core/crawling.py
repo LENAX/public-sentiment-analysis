cdec001a5bf12e2c9652db0093e63e667f6de04e
stevelan <stevelan1995@gmail.com>
2021-06-11 15:46:51 +0800

    created crawling strategy module

--- spider/app/core/crawling.py
+++ spider/app/core/crawling.py
@@ -7,5 +7,22 @@ class BaseCrawlingStrategy(ABC):
     """ Base strategy for crawling websites
     """
 
-    async def crawl(self, rules: List[object]) -> List[object]:
+    async def crawl(self, rules: List[object]):
         return NotImplemented
+
+
+class CrawlerContext(object):
+
+    def __init__(self, crawling_strategy: BaseCrawlingStrategy):
+        self._crawling_strategy = crawling_strategy
+
+    @property
+    def crawling_strategy(self) -> BaseCrawlingStrategy:
+        return self._crawling_strategy
+
+    @crawling_strategy.setter
+    def crawling_strategy(self, crawling_strategy: BaseCrawlingStrategy) -> None:
+        self._crawling_strategy = crawling_strategy
+
+    async def crawl(self, rules: List[object]) -> List[object]:
+        return self._crawling_strategy.crawl(rules)
