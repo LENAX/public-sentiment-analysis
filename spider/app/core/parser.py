6a74dc8b31c1edc28ebc797ba6b33bc441fc54c5
stevelan <stevelan1995@gmail.com>
2021-06-11 15:44:02 +0800

    code cleanup

--- spider/app/core/parser.py
+++ spider/app/core/parser.py
@@ -105,21 +105,6 @@ class LinkParser(BaseParsingStrategy):
         return list(parsed_links)
 
 
-"""
-public class Context {
-   private Strategy strategy;
-
-   public Context(Strategy strategy){
-      this.strategy = strategy;
-   }
-
-   public int executeStrategy(int num1, int num2){
-      return strategy.doOperation(num1, num2);
-   }
-}
-"""
-
-
 class ParserContext(object):
 
     def __init__(self, parsing_strategy: BaseParsingStrategy):
