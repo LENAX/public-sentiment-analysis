0bee7209f1b576e2d20269309663a716b0b3eb2c
stevelan <stevelan1995@gmail.com>
2021-06-16 15:04:39 +0800

    parsing text with multiprocessing

--- spider/app/core/concurrency_demos/background_runner.py
+++ spider/app/core/concurrency_demos/background_runner.py
@@ -1,38 +1,86 @@
 import asyncio
 import aiohttp
+from lxml.html import fromstring
+from functools import partial
 from concurrent.futures import ProcessPoolExecutor
-from numba import jit
-import numpy as np
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+import os
+
+@dataclass
+class SearchResult:
+    title: str
+    href: str
+    abstract: str
+    date: str
+    image_urls: List[str]
+
 
 """ Running a cpu bound task and an io bound task concurrently
 """
 
 
-def cpu_bound():
+def parsing(text):
     # CPU-bound operations will block the event loop:
     # in general it is preferable to run them in a
     # process pool.
-    print("doing computation")
-    n = np.zeros((1000,1000), dtype=complex)
-    n[60:80, 20:40] = np.exp(1j*np.random.uniform(0, 2*np.pi, (20, 20)))
-    im = np.fft.ifftn(n).real
-    return im
+    my_pid = os.getpid()
+    print(f"My id is: {my_pid}")
+    search_result_xpath = "//div[contains(@class, 'result') and contains(@class, 'new-pmd')]"
+    title_link_xpath = "//h3/a[not (@class)]"
+    abstract_xpath = "//div[contains(@class, 'c-abstract')]"
+    datetime_xpath = "//span[contains(@class, '_3wnyfua')]"
+    image_src_xpath = "//span[contains(@class, 'c-img-border')]//preceding-sibling::img/@src"
+
+    parsed_text = fromstring(text)
+    search_results = parsed_text.xpath(search_result_xpath)
+    
+    parsed_search_results = []
+    for result in search_results:
+        title_link = result.xpath(title_link_xpath)
+        abstract = result.xpath(abstract_xpath)
+        create_dt = result.xpath(datetime_xpath)
+        image_srcs = result.xpath(image_src_xpath)
+
+        title, href, abstract_text, create_date, image_sources = "", "", "", None, []
+        if len(title_link):
+            title = title_link[0].text_content().strip()
+            href = title_link[0].get('href')
+        
+        if len(abstract):
+            abstract_text = abstract[0].text_content().strip()
+        
+        if len(create_dt):
+            create_date = create_dt[0].text_content().strip()
+        
+        for src in image_srcs:
+            image_sources.append(str(src))
+
+        parsed_search_results.append(SearchResult(
+            title=title, href=href, abstract=abstract_text,
+            date=create_date, image_urls=image_sources
+        ))
+    
+    return parsed_search_results
 
 
 async def http_requests(urls):
     print("doing io")
-    async def fetch(request_client, url):
-        async with request_client.get(url) as response:
+    async def fetch(request_client, url, semaphore):
+        async with semaphore, request_client.get(url) as response:
             result = await response.text()
 
         return result
-
-    async with aiohttp.ClientSession() as request_client:
-        concurrent_fetch_tasks = [asyncio.create_task(fetch(request_client, url)) for url in urls]
+    semaphore = asyncio.Semaphore(3)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
+    async with aiohttp.ClientSession(headers=headers) as request_client:
+        concurrent_fetch_tasks = [asyncio.create_task(fetch(request_client, url, semaphore)) for url in urls]
         results = await asyncio.gather(*concurrent_fetch_tasks)
 
     print(len(results))
-
     return results
 
 
@@ -40,27 +88,31 @@ async def http_requests(urls):
 async def main(cpu_bound_task, async_task, **kwargs):
     loop = asyncio.get_running_loop()
 
-    async def compute():
-        with ProcessPoolExecutor(max_workers=4) as pool:
-            result = await loop.run_in_executor(pool, cpu_bound_task)
-            print(f"Result: {result}")
+    async def parse(text_list):
+        with ProcessPoolExecutor() as pool:
+            futures = [loop.run_in_executor(pool, partial(cpu_bound_task, text))
+                       for text in text_list]
+            print(futures)
+            result = await asyncio.gather(*futures)
+        return result
+    # print(urls)
+    fetch_results = await async_task(kwargs['urls'])
+    parse_results = await parse(fetch_results)
 
-    await asyncio.gather(
-        asyncio.create_task(compute()),
-        asyncio.create_task(async_task(kwargs['urls']))
-    )
+    print(parse_results)
+    return parse_results
 
     # io_results = await async_task(kwargs["urls"])
     # print(len(io_results))
 
 
 urls=[
-    f"https://www.baidu.com/s?wd=asyncio&pn={i}&rsv_spt=1&rsv_iqid=0x8532419c002843d7&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_dl=tb&rsv_enter=1&rsv_sug3=8&rsv_sug1=7&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&inputT=1935&rsv_sug4=2662"
-    for i in range(0, 1000, 10)
+    f"https://www.baidu.com/s?wd=asyncio&pn={i*10}"
+    for i in range(0, 20)
 ]
 
-# asyncio.run(http_requests(urls))
+asyncio.run(http_requests(urls))
 
 asyncio.run(
-    main(cpu_bound, http_requests, urls=urls)
+    main(parsing, http_requests, urls=urls)
 )
