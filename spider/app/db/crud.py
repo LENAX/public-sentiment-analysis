9d3ac4f85d4bff656f1ce16e81c5f97206a3700a
stevelan <stevelan1995@gmail.com>
2021-06-22 09:57:08 +0800

    update crud base

--- spider/app/db/crud.py
+++ spider/app/db/crud.py
@@ -1,22 +1,24 @@
 """ CRUD Base Class
 """
+from abc import ABC, abstractmethod
 from typing import Any, List
 
 
-class AsyncCRUDBase(object):
+class AsyncCRUDBase(ABC):
 
-    @staticmethod
+    @abstractmethod
     async def get(db: Any, query: Any, **kwargs):
         return NotImplemented
 
-    @staticmethod
+    @abstractmethod
     async def delete(db: Any, query: Any, **kwargs):
         return NotImplemented
 
-    @staticmethod
+    @abstractmethod
     async def insert_many(db: Any, data: Any, **kwargs):
         return NotImplemented
 
+    @abstractmethod
     async def save(self, db, collection):
         return NotImplemented
 
@@ -27,16 +29,16 @@ class AsyncMongoCRUDBase(AsyncCRUDBase):
     
     @staticmethod
     async def get(collection: Any,  query: Any, **kwargs) -> List[object]:
-        result = [data async for data in collection.find(query)]
+        cursor = collection.find(query)
+        result = await cursor.to_list()
         return result
 
     @staticmethod
-    async def delete(collection: Any, query: Any, **kwargs):
-        return NotImplemented
+    async def delete(collection: Any, query: Any, **kwargs) -> None:
+        result = await collection.delete_many(query)
+        print(f"Deleted {result.deleted_count} items.")
 
     @staticmethod
-    async def insert_many(collection: Any, data: Any, **kwargs):
+    async def insert_many(collection: Any, data: Any, **kwargs) -> None:
         await collection.insert_many(data)
 
-    async def save(self, collection):
-        return NotImplemented
