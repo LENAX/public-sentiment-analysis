6b89694ac1c26e9e9c0594d85581ab3142b68107
stevelan <stevelan1995@outlook.com>
2021-09-30 00:28:20 +0800

    add test driver

--- spider_services/baidu_news_spider/app/service.py
+++ spider_services/baidu_news_spider/app/service.py
@@ -2,8 +2,9 @@ import logging
 import re
 from datetime import datetime, timedelta
 from logging import Logger
-from spider_services.common.models.data_models.parser_models import ParseResult
 from typing import Callable, List, Tuple
+
+from spider_services.common.models.data_models.parser_models import ParseResult
 
 from ...common.core import (BaseRequestClient, BaseSpider, ParserContext,
                             ParserContextFactory)
@@ -268,3 +269,126 @@ class BaiduNewsSpider(BaseSpiderService):
 
         except Exception as e:
             print('An exception occurred')
+
+
+
+def make_cookies(cookie_text):
+    cookie_strings = cookie_text.replace("\n","").replace(" ","").split(";")
+    cookies = {}
+    for cookie_str in cookie_strings:
+        try:
+            key, value = cookie_str.split("=")
+            cookies[key] = value
+        except IndexError:
+            print(cookie_str)
+        except ValueError:
+            print(cookie_str)
+    return cookies
+
+if __name__ == "__main__":
+    import asyncio
+    from os import getcwd
+    from typing import Any
+
+    from devtools import debug
+    from motor.motor_asyncio import AsyncIOMotorClient
+    from yaml import CDumper as Dumper
+    from yaml import CLoader as Loader
+    from yaml import dump, load
+
+    from ...common.core import CrawlerContextFactory, RequestClient, Spider
+    from ...common.models.data_models import RequestHeader
+    from ...common.models.request_models import (KeywordRules, ParseRule,
+                                                 ParsingPipeline, ScrapeRules,
+                                                 TimeRange)
+
+
+    def create_client(host: str, username: str,
+                      password: str, port: int,
+                      db_name: str) -> AsyncIOMotorClient:
+        return AsyncIOMotorClient(
+            f"mongodb://{username}:{password}@{host}:{port}/{db_name}?authSource=admin")
+
+
+    def load_service_config(
+        config_name: str,
+        loader_func: Callable=load,
+        loader_class: Any=Loader,
+        config_class: Any = ScrapeRules,
+        config_path: str = f"{getcwd()}/spider/app/service_configs"
+    ) -> object:
+        with open(f"{config_path}/{config_name}.yml", "r") as f:
+            config_text = f.read()
+            parsed_obj = loader_func(config_text, Loader=loader_class)
+            config_obj = config_class.parse_obj(parsed_obj)
+            return config_obj
+
+    def save_config(config, path, dump: Any = dump, dump_class: Any = Dumper):
+        with open(path, 'w+') as f:
+            config_text = dump(config, Dumper=dump_class)
+            f.write(config_text)
+
+    async def test_spider_services(db_client,
+                                   db_name,
+                                   headers,
+                                   cookies,
+                                   client_session_class,
+                                   spider_class,
+                                   parse_strategy_factory,
+                                   crawling_strategy_factory,
+                                   spider_service_class,
+                                   result_model_class,
+                                   test_urls,
+                                   rules):
+        db = db_client[db_name]
+        result_model_class.db = db
+
+        async with (await client_session_class(headers=headers, cookies=cookies)) as client_session:
+            spider_service = spider_service_class(request_client=client_session,
+                                                  spider_class=spider_class,
+                                                  parse_strategy_factory=parse_strategy_factory,
+                                                  crawling_strategy_factory=crawling_strategy_factory,
+                                                  result_db_model=result_model_class)
+            await spider_service.crawl(test_urls, rules)
+
+    cookie_text = """BIDUPSID=C2730507E1C86942858719FD87A61E58;
+    PSTM=1591763607; BAIDUID=0145D8794827C0813A767D21ADED26B4:FG=1;
+    BDUSS=1jdUJiZUIxc01RfkFTTUtoTXZaSFl1SDlPdEgzeGJGVEhkTDZzZ2ZIZlJSM1ZmSVFBQUFBJCQAAAAAAAAAAAEAAACILlzpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANG6TV~Ruk1fek;
+    __yjs_duid=1_9e0d11606e81d46981d7148cc71a1d391618989521258; BD_UPN=123253; BCLID_BFESS=7682355843953324419; BDSFRCVID_BFESS=D74OJeC6263c72vemTUDrgjXg2-lavcTH6f3bGYZSp4POsT0C6gqEG0PEf8g0KubxY84ogKK3gOTH4PF_2uxOjjg8UtVJeC6EG0Ptf8g0f5;
+    H_BDCLCKID_SF_BFESS=tbu8_IIMtCI3enb6MJ0_-P4DePop3MRZ5mAqoDLbKK0KfR5z3hoMK4-qWMtHe47KbD7naIQDtbonofcbK5OmXnt7D--qKbo43bRTKRLy5KJvfJo9WjAMhP-UyNbMWh37JNRlMKoaMp78jR093JO4y4Ldj4oxJpOJ5JbMonLafD8KbD-wD5LBeP-O5UrjetJyaR3R_KbvWJ5TMC_CDP-bDRK8hJOP0njM2HbMoj6sK4QjShPCb6bDQpFl0p0JQUReQnRm_J3h3l02Vh5Ie-t2ynLV2buOtPRMW20e0h7mWIbmsxA45J7cM4IseboJLfT-0bc4KKJxbnLWeIJIjj6jK4JKDG8ft5OP;
+    """
+    cookies = make_cookies(cookie_text)
+
+    headers = RequestHeader(
+        accept="text/html, application/xhtml+xml, application/xml, image/webp, */*",
+        user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+        )
+    use_db = 'test'
+    db_client = create_client(host='localhost',
+                              username='admin',
+                              password='root',
+                              port=27017,
+                              db_name=use_db)
+    urls = [
+        "http://www.baidu.com/s?tn=news&ie=utf-8"
+    ]
+    print(urls)
+    config = load_service_config("aqi_config")
+    debug(config)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test_spider_services(
+        db_client=db_client,
+        db_name=use_db,
+        headers=headers.dict(),
+        cookies=cookies,
+        client_session_class=RequestClient,
+        spider_class=Spider,
+        parse_strategy_factory=ParserContextFactory,
+        crawling_strategy_factory=CrawlerContextFactory,
+        spider_service_class=BaiduNewsSpider,
+        result_model_class=NewsDBModel,
+        test_urls=urls,
+        rules=config
+    ))
+
